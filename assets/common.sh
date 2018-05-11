233894553598fc1adab29d6f87128a1832d72dd3
Shishir Amin <samin@pivotal.io>
2018-05-11 17:44:31 -0400

    fix jq path issues in artifact list

--- assets/common.sh
+++ assets/common.sh
@@ -35,7 +35,7 @@ get_current_version() {
   local regex=$1
   local folder_contents=$2
 
-  echo "$folder_contents" | jq --arg v "$regex" '[.children[].path | capture($v)]' | jq 'sort_by(.version | split(".") | map(tonumber))' | jq '[.[length-1] | {version: .version}]'
+  echo "$folder_contents" | jq --arg v "$regex" '[.[].path | capture($v)]' | jq 'sort_by(.version | split(".") | map(tonumber))' | jq '[.[length-1] | {version: .version}]'
 }
 
 # Return all versions
@@ -43,14 +43,14 @@ get_all_versions() {
   local regex=$1
   local folder_contents=$2
 
-  echo "$folder_contents" | jq --arg v "$regex" '[.children[].path | capture($v)]' | jq 'sort_by(.version | split(".") | map(tonumber))' | jq '[.[] | {version: .version}]'
+  echo "$folder_contents" | jq --arg v "$regex" '[.[].path | capture($v)]' | jq 'sort_by(.version | split(".") | map(tonumber))' | jq '[.[] | {version: .version}]'
 }
 
 get_files() {
   local regex="(?<path>$1)"
   local folder_contents=$2
 
-  echo "$folder_contents" | jq --arg v "$regex" '[.children[].path | capture($v)]' | jq 'sort_by(.version  | split(".") | map(tonumber))' | jq '[.[] | {path: .path, version: .version}]'
+  echo "$folder_contents" | jq --arg v "$regex" '[.[].path | capture($v)]' | jq 'sort_by(.version  | split(".") | map(tonumber))' | jq '[.[] | {path: .path, version: .version}]'
 }
 
 artifactory_get_folder_contents() {
